246dd50f727dce0587f78fa90f5c6eb0e27140cb
MatadoxCode <eliasdossantos200@gmail.com>
2025-10-30 22:23:49 +0000

    xadrez - Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -11,25 +11,38 @@ int main() {
 // Torre - Andar cinco casas para direita While.
     while (torre<= 5)
     {
-        printf("Torre andou %d casa para direita\n", torre);
+        printf("Torre andou 1 casa para direita\n");
         torre++;
     }
 
 printf("\n");
+
 // Bispo - Andar cinco casas na diagonal para cima e direita usando do While
     do
     {
-        printf("Bispo andou %d diagonalmente para cima direita\n", bispo);
+        printf("Bispo andou 1 diagonalmente para cima direita\n");
         bispo++;
     } while (bispo <= 5);
 printf("\n");
+
 // Rainha - Andar oito casas para a esquerda usando For.
 
     for(rainha ; rainha <= 8; rainha++){
-        printf("Rainha andou %d casa para a esquerda\n", rainha);
+        printf("Rainha andou 1 casa para a esquerda\n");
     }
+printf("\n");
 
+// Cavalo - Andar duas casas para cima e uma direita usando Loop aninhados.
+    int cavalo = 1;
 
+    while (cavalo < 2)
+    {
+        for(cavalo; cavalo <= 2; cavalo++){
+            printf("Cavalo andou %d casa para cima\n", cavalo);
+        }
+        printf("Cavalo andou 1 casa para direita\n");
+    }
+    
 
 
     return 0;
