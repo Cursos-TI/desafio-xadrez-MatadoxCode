45299b1fe33abf1e4f61d70412d6a6f01c033b2f
MatadoxCode <eliasdossantos200@gmail.com>
2025-10-31 01:26:31 +0000

    Xadrez - Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Recursivos para torre, bispo e rainha.
+void torre(int numero){
+    if(numero > 0){
+        printf("Torre andou 1 casa para direita\n");
+        torre(numero - 1);
+    }
+}
+
+void bispo (int numero){
+    
+    if(numero > 0){
+        for(int bispo1 = 1; bispo1<= 5; bispo1++){
+            printf("Bispo andou uma casa vertical\n");
+                for(int bispo2 = 1; bispo2 <2; bispo2++){
+                    printf("Bispo andou uma casa na horizontal\n");
+                }
+    }
+}
+}
+void rainha(int numero){
+    if(numero >0){
+        printf("Rainha andou 1 casa para a esquerda\n");
+        rainha(numero - 1);
+    }
+}
 int main() {
 
-    int torre = 1, bispo = 1, rainha = 1;
+// Chamando os recursivos da torre, bispo e rainha.   
 
-// Torre - Andar cinco casas para direita While.
-    while (torre<= 5)
-    {
-        printf("Torre andou 1 casa para direita\n");
-        torre++;
-    }
+    torre(5); // Torre - Andar cinco casas para direita. 
+    printf("\n");
 
-printf("\n");
+    bispo(5); // Bispo - Andar cinco casas na diagonal para cima e direita.
+    printf("\n");
 
-// Bispo - Andar cinco casas na diagonal para cima e direita usando do While
-    do
-    {
-        printf("Bispo andou 1 diagonalmente para cima direita\n");
-        bispo++;
-    } while (bispo <= 5);
-printf("\n");
+    rainha(5);// Rainha - Andar oito casas para a esquerda.
+    printf("\n");
 
-// Rainha - Andar oito casas para a esquerda usando For.
-
-    for(rainha ; rainha <= 8; rainha++){
-        printf("Rainha andou 1 casa para a esquerda\n");
-    }
-printf("\n");
-
-// Cavalo - Andar duas casas para cima e uma direita usando Loop aninhados.
-    int cavalo = 1;
-
-    while (cavalo < 2)
-    {
-        for(cavalo; cavalo <= 2; cavalo++){
-            printf("Cavalo andou %d casa para cima\n", cavalo);
+// Cavalo - Andar duas casas para cima e uma para direita usando loops aninhados com multiplas variaveis.
+    for(int cavalo = 1; cavalo < 2; cavalo ++ ){
+        for(int cavalo1 = 1; cavalo1 <= 2; cavalo1 ++){
+            printf("Cavalo andou 1 casa para cima\n");
         }
         printf("Cavalo andou 1 casa para direita\n");
     }
